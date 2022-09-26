b3b11328f42402b674c49c0b66507bc943e0da27
juanesherrera21 <juanesherrera21@hotmail.com>
2022-09-26 14:50:38 -0500

    Se añadio la navegacion de creditos a volver a la main activity

--- app/src/main/java/com/example/mainactivity/creditos.java
+++ app/src/main/java/com/example/mainactivity/creditos.java
@@ -1,11 +1,20 @@
 package com.example.mainactivity;
 
 import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class creditos extends AppCompatActivity {
+
+    public void onClick(View view){
+
+        Intent miIntent = new Intent(creditos.this,MainActivity.class);
+
+        startActivity(miIntent);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
